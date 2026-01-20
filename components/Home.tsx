1dc9eac9b339bdd430da11d9898eaf055411b368
Yamin Mushtaq <yaminmushtaqr@gmail.com>
2026-01-20 17:41:46 +0000

    removed techstack

--- components/Home.tsx
+++ components/Home.tsx
@@ -2,13 +2,13 @@ import React from 'react';
 import { ChipScroll } from './ChipScroll';
 import { Roadmap } from './Roadmap';
 import { Features } from './Features';
-import { TechStack } from './TechStack';
+
 
 export const Home: React.FC = () => {
   return (
     <>
       <ChipScroll />
-      <TechStack />
+
       <Roadmap />
       <Features />
     </>
